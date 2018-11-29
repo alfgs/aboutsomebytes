2b786c7a8bc9ccf71f0e8accbc68a78c15abfd67
alfgs <44975559+alfgs@users.noreply.github.com>
2018-11-29 16:49:23 +0200

    Add files via upload

---
+++ popaandreea_2set5.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int n;
+int main()
+{
+
+    cout<<"n=";cin>>n;
+    int=x;
+   while(n>0)
+         {
+             n=n>>1;
+             x=x+1;}
+   cout<<x;
+   return 0;
+}
+
+         }
+}
